516bfe4980b8f83bda6b3f22d77b3b983a1c9d8f
Daniel Kowalski <77574206+k0wal45@users.noreply.github.com>
2023-01-13 18:32:26 +0100

    Add files via upload

---
+++ js/boxApear.js
@@ -0,0 +1,27 @@
+const firstButton = document.getElementById('firstBoxBtn');
+
+const FirstBox = document.getElementById('firstBox')
+
+const
+
+if( FirstBox.querySelector('.hide')) {
+    firstButton.onclick = firstBoxAppear
+} else if (FirstBox.querySelector('.active')) {
+    firstButton.onclick = firstBoxHide
+}
+
+
+
+const secondButton = document.getElementById('secondBoxBtn');
+
+
+
+const thirdButton = document.getElementById('thirdBoxBtn');
+
+
+
+
+
+
+
+
