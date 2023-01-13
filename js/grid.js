717d3c04eb8234546bf5875f9ae0f2fc13e4ec5f
Daniel Kowalski <77574206+k0wal45@users.noreply.github.com>
2023-01-13 15:10:28 +0100

    Add files via upload

--- js/grid.js
+++ js/grid.js
@@ -4,24 +4,7 @@ let columns = 0,
         rows = 0,
         toggled = false;
 
-const toggle = () => {
-        toggled = !toggled;
-        
-        document.body.classList.toggle("toggled");
-}
 
-const handleOnClick = index => {
-        toggle();
-        
-        anime({
-        targets: ".tile",
-        opacity: toggled ? 0 : 1,
-        delay: anime.stagger(50, {
-        grid: [columns, rows],
-        from: index
-        })
-        });
-}
 
 const createTile = index => {
         const tile = document.createElement("div");
@@ -44,10 +27,10 @@ const createTiles = quantity => {
 const createGrid = () => {
         wrapper.innerHTML = "";
         
-        const size = document.body.clientWidth > 800 ? 100 : 50;
+        const size = document.body.clientWidth > 800 ? 80 : 50;
         
         columns = Math.floor(document.body.clientWidth / size);
-        rows = Math.floor(document.body.clientHeight / size);
+        rows = Math.floor(document.body.clientHeight / size/2);
         
         wrapper.style.setProperty("--columns", columns);
         wrapper.style.setProperty("--rows", rows);
